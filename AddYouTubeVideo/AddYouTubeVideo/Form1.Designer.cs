107888f2da045b81c47958d8bea081323d6b89bf
Civoremo <omerovic_n@hotmail.com>
2016-03-13 15:53:01 -0400

    YouTube video AddOn for PLMP
    Add links of favorite Youtube videos to your PLMP player.

---
+++ AddYouTubeVideo/AddYouTubeVideo/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace AddYouTubeVideo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBxName = new System.Windows.Forms.TextBox();
+            this.txtBxAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAddVideo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBxName
+            // 
+            this.txtBxName.Location = new System.Drawing.Point(50, 25);
+            this.txtBxName.Name = "txtBxName";
+            this.txtBxName.Size = new System.Drawing.Size(423, 20);
+            this.txtBxName.TabIndex = 0;
+            // 
+            // txtBxAddress
+            // 
+            this.txtBxAddress.Location = new System.Drawing.Point(50, 74);
+            this.txtBxAddress.Name = "txtBxAddress";
+            this.txtBxAddress.Size = new System.Drawing.Size(423, 20);
+            this.txtBxAddress.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name Of Video";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Url / Web Address";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnAddVideo
+            // 
+            this.btnAddVideo.Location = new System.Drawing.Point(169, 106);
+            this.btnAddVideo.Name = "btnAddVideo";
+            this.btnAddVideo.Size = new System.Drawing.Size(151, 39);
+            this.btnAddVideo.TabIndex = 4;
+            this.btnAddVideo.Text = "Add Video To Collection";
+            this.btnAddVideo.UseVisualStyleBackColor = true;
+            this.btnAddVideo.Click += new System.EventHandler(this.btnAddVideo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(482, 157);
+            this.Controls.Add(this.btnAddVideo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBxAddress);
+            this.Controls.Add(this.txtBxName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Add YouTube Video App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBxName;
+        private System.Windows.Forms.TextBox txtBxAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAddVideo;
+    }
+}
+
---
+++ AddYouTubeVideo/AddYouTubeVideo/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AddYouTubeVideo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddVideo_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveNameToFolder = new SaveFileDialog();
+            if (!File.Exists(@"C:\AddedVideoFiles\YouTubeVideoNames.txt"))
+            {
+                Directory.CreateDirectory(@"C:\AddedVideoFiles");
+                File.Create(@"C:\AddedVideoFiles\YouTubeVideoNames.txt");
+                
+            }
+            if (!File.Exists(@"C:\AddedVideoFiles\YouTubeVideoAddress.txt"))
+            {
+                File.Create(@"C:\AddedVideoFiles\YouTubeVideoAddress.txt");
+            }
+            
+            saveNameToFolder.FileName = @"C:\AddedVideoFiles\YouTubeVideoNames.txt";
+            string nameFolder = saveNameToFolder.FileName;
+            saveNameToFolder.Filter = "Text File |*.txt";
+
+
+            if (txtBxName.Text != "" && txtBxAddress.Text != "")
+            {
+                using (StreamWriter swName = File.AppendText(nameFolder))
+                {
+                    swName.WriteLine(txtBxName.Text.ToString());
+                    swName.Flush();
+                }
+
+            }
+
+
+            SaveFileDialog saveAddressToFolder = new SaveFileDialog();
+
+            saveAddressToFolder.FileName = @"C:\AddedVideoFiles\YouTubeVideoAddress.txt";
+            string addressFolder = saveAddressToFolder.FileName;
+            saveAddressToFolder.Filter = "Text File |*.txt";
+
+            if (txtBxName.Text != "" && txtBxAddress.Text != "")
+            {
+                using (StreamWriter swAddress = File.AppendText(addressFolder))
+                {
+                    string newAddress = txtBxAddress.Text.Replace("watch?v=", "v/");
+                    swAddress.WriteLine(newAddress.ToString());
+                    swAddress.Flush();
+                }
+            }
+
+            txtBxAddress.Text = "";
+            txtBxName.Text = "";
+            
+
+        }
+    }
+}
